5f72536dd71575ff6d45f433c5ec345d718f3039
jsotera <125059957+jsotera@users.noreply.github.com>
2023-03-05 23:38:36 +0100

    solución al proyecto de Estadistica

--- src/main/java/es/mariaanasanz/proyecto6/ejercicios/Estadisticas.java
+++ src/main/java/es/mariaanasanz/proyecto6/ejercicios/Estadisticas.java
@@ -2,15 +2,14 @@ package es.mariaanasanz.proyecto6.ejercicios;
 
 import javafx.scene.input.KeyCode;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
+import java.util.*;
+import java.util.Map.Entry;
 
 public class Estadisticas {
 
-    private static LinkedHashMap<KeyCode, Integer> contadorEventosTeclado;
-    private static HashMap<String, HashMap<String, Integer>> contadorObjetosRecogidos;
-    private static ArrayList<Boolean> historicoDisparos;
+    private static LinkedHashMap<KeyCode, Integer> contadorEventosTeclado = new LinkedHashMap<>();
+    private static HashMap<String, HashMap<String, Integer>> contadorObjetosRecogidos = new HashMap<>();
+    private static ArrayList<Boolean> historicoDisparos = new ArrayList<>();
 
     public static void mostrarEstadisticasSeguro(){
         try {
@@ -66,7 +65,34 @@ public class Estadisticas {
      * @param code el cogido de evento capturado
      */
     public static void capturarEventoTeclado(KeyCode code) {
-
+        if(code==null){
+            return;
+        }
+        Integer contador;
+        switch (code){
+            case RIGHT:
+                code = KeyCode.RIGHT;
+                break;
+            case LEFT:
+                code = KeyCode.LEFT;
+                break;
+            case SHIFT:
+                code = KeyCode.SHIFT;
+                break;
+            case ESCAPE:
+                code = KeyCode.ESCAPE;
+                break;
+            default:
+                code = KeyCode.ASTERISK;
+                break;
+        }
+        contador = contadorEventosTeclado.get(code);
+        if(contador!=null){
+            contador = Integer.valueOf(contador.intValue()+1);
+        } else {
+            contador = Integer.valueOf(1);
+        }
+        contadorEventosTeclado.put(code, contador);
     }
 
     /**
@@ -82,7 +108,18 @@ public class Estadisticas {
      * IMPORTANTE: Se debera emplear StringBuilder para construir la cadena a mostrar
      */
     public static void mostrarEventosTeclado(){
-
+        StringBuilder sb = new StringBuilder("Teclas pulsadas durante la partida:");
+        Set<Entry<KeyCode, Integer>> entries = contadorEventosTeclado.entrySet();
+        for (Entry<KeyCode, Integer> entry : entries) {
+            Integer contador = entry.getValue();
+            sb.append("\n\t- "+entry.getKey().toString()+": "+contador.toString());
+            if(contador.intValue()==1){
+                sb.append(" vez");
+            } else {
+                sb.append(" veces");
+            }
+        }
+        System.out.println(sb.toString());
     }
 
     /**
@@ -92,7 +129,16 @@ public class Estadisticas {
      * @return KeyCode mas frecuente
      */
     public static KeyCode teclaMasPulsada(){
-        return null;
+        Set<KeyCode> keys = contadorEventosTeclado.keySet();
+        KeyCode teclaMasPulsada = KeyCode.ESCAPE;
+        int maxOcurrencias = 0;
+        for (KeyCode key : keys) {
+            if(maxOcurrencias<contadorEventosTeclado.get(key)){
+                maxOcurrencias = contadorEventosTeclado.get(key);
+                teclaMasPulsada = key;
+            }
+        }
+        return teclaMasPulsada;
     }
 
     /**
@@ -104,7 +150,17 @@ public class Estadisticas {
      * @param objeto sera o la comida o la gema
      */
     public static void objetoRecogido(String actor, String objeto){
-
+        HashMap<String, Integer> objetosRecogidos = contadorObjetosRecogidos.get(actor);
+        if(objetosRecogidos==null){
+            objetosRecogidos = new HashMap<>();
+        }
+        Integer cantidadRecogida = objetosRecogidos.get(objeto);
+        if(cantidadRecogida==null){
+            cantidadRecogida = Integer.valueOf(0);
+        }
+        cantidadRecogida = Integer.valueOf(cantidadRecogida.intValue()+1);
+        objetosRecogidos.put(objeto, cantidadRecogida);
+        contadorObjetosRecogidos.put(actor, objetosRecogidos);
     }
 
     /**
@@ -121,7 +177,22 @@ public class Estadisticas {
      * IMPORTANTE: Se debera emplear StringBuilder para construir la cadena a mostrar
      */
     public static void mostrarObjetosRecogidos(){
-
+        StringBuilder sb = new StringBuilder("Objetos recogidos durante la partida:");
+        Set<String> keys = contadorObjetosRecogidos.keySet();
+        for(String key : keys){
+            Set<Entry<String, Integer>> entries = contadorObjetosRecogidos.get(key).entrySet();
+            sb.append("\n\t- "+key.toUpperCase()+":");
+            for (Entry<String, Integer> entry : entries) {
+                Integer contador = entry.getValue();
+                sb.append("\n\t\t- "+entry.getKey()+": "+contador.toString());
+                if(contador.intValue()==1){
+                    sb.append(" vez");
+                } else {
+                    sb.append(" veces");
+                }
+            }
+        }
+        System.out.println(sb.toString());
     }
 
     /**
@@ -132,7 +203,32 @@ public class Estadisticas {
      * IMPORTANTE: Se debera emplear StringBuilder para construir la cadena a mostrar
      */
     public static void mostrarQuienHaRecogidoMasObjetos(){
-
+        StringBuilder sb = new StringBuilder("Quien ha recogido mas objetos ha sido... ");
+        Set<String> keys = contadorObjetosRecogidos.keySet();
+        int cantidadRecogida = 0;
+        String actor = null;
+        for(String key : keys){
+            Collection<Integer> coleccionContador = contadorObjetosRecogidos.get(key).values();
+            int auxCantidadRecogida = 0;
+            for (Integer cantidad : coleccionContador) {
+                auxCantidadRecogida = auxCantidadRecogida + cantidad.intValue();
+            }
+            if(cantidadRecogida<auxCantidadRecogida){
+                cantidadRecogida = auxCantidadRecogida;
+                actor = key;
+            }
+        }
+        if(actor!=null){
+            sb.append("¡"+actor.toUpperCase()+" con un total de "+cantidadRecogida+" ");
+            if(cantidadRecogida==1){
+                sb.append("objeto!");
+            } else {
+                sb.append("objetos!");
+            }
+        } else {
+            sb.append("¡NADIE! No se han recogido objetos...");
+        }
+        System.out.println(sb.toString());
     }
 
     /**
@@ -142,7 +238,7 @@ public class Estadisticas {
      * @param exito representa si el disparo es certero (true) o fallido (false)
      */
     public static void capturarDisparo(boolean exito){
-
+        historicoDisparos.add(Boolean.valueOf(exito));
     }
 
     /**
@@ -152,20 +248,41 @@ public class Estadisticas {
      * @param exito representa si el disparo es certero (true) o fallido (false)
      */
     public static void borrarDisparo(boolean exito){
-
+        int index = historicoDisparos.lastIndexOf(Boolean.valueOf(exito));
+        if(index>=0){
+            historicoDisparos.remove(index);
+        }
     }
 
     /**
      * TODO: Se debera mostrar por consola el porcentaje de precision en base al contenido del ArrayList historicoDisparos con el siguiente formato: (8 puntos)
      * Tienes una precision del [XX]%.
      * Adicionalmente, en base a la precision, se mostrara un mensaje adicional:
-     *      Si la precion es entre un 67 y un 100% --> ¡Eres insuperable!
-     *      Si la precion es entre un 34 y un 66% --> No esta nada mal
-     *      Si la precion es entre un 0 y un 33% --> Deberias entrenar un poco mas...
+     *      Si la precision es entre un 67 y un 100% --> ¡Eres insuperable!
+     *      Si la precision es entre un 34 y un 66% --> No esta nada mal
+     *      Si la precision es entre un 0 y un 33% --> Deberias entrenar un poco mas...
      * IMPORTANTE: La precision se mide base a los disparos acertados entre el total de disparos
      * IMPORTANTE: Se debera emplear StringBuilder para construir la cadena a mostrar
      */
     public static void mostrarRatioPrecision(){
-
+        int aciertos = 0;
+        for(Boolean exito : historicoDisparos){
+            if(exito.booleanValue()){
+                aciertos++;
+            }
+        }
+        int precision = 0;
+        if(aciertos>0){
+            precision = (aciertos*100)/historicoDisparos.size();
+        }
+        StringBuilder sb = new StringBuilder("Tienes una precision del "+precision+"%. ");
+        if(precision<34){
+            sb.append("Deberias entrenar un poco mas...");
+        } else if (precision<67) {
+            sb.append("No esta nada mal");
+        } else {
+            sb.append("¡Eres insuperable!");
+        }
+        System.out.println(sb.toString());
     }
 }
--- src/main/java/es/mariaanasanz/proyecto6/simulador/Simulador.java
+++ src/main/java/es/mariaanasanz/proyecto6/simulador/Simulador.java
@@ -24,7 +24,71 @@ public class Simulador {
      *             numJoyaJugador numComidaZarigueya numJoyaZarigueya numDisparosCerteros numDisparosFallidos numDisparosCerterosBorrar numDisparosFallidosBorrar
      */
     public static void main(String[] args) {
-
+        if(args.length!=13){
+            System.out.println("Lo siento, no has introducido la cantidad de parametros necesarios para la simulacion.");
+            return;
+        }
+        for(String arg : args){
+            try {
+                Integer.parseInt(arg);
+            } catch (NumberFormatException e) {
+                System.out.println("Lo siento, todos los parametros deben ser numericos.");
+                return;
+            }
+        }
+        for (int i = 0; i < Integer.parseInt(args[0]); i++) {
+            Estadisticas.capturarEventoTeclado(KeyCode.LEFT);
+        }
+        for (int i = 0; i < Integer.parseInt(args[1]); i++) {
+            Estadisticas.capturarEventoTeclado(KeyCode.RIGHT);
+        }
+        for (int i = 0; i < Integer.parseInt(args[2]); i++) {
+            Estadisticas.capturarEventoTeclado(KeyCode.SHIFT);
+        }
+        for (int i = 0; i < Integer.parseInt(args[3]); i++) {
+            Estadisticas.capturarEventoTeclado(KeyCode.ESCAPE);
+        }
+        for (int i = 0; i < Integer.parseInt(args[4]); i++) {
+            Estadisticas.capturarEventoTeclado(KeyCode.ASTERISK);
+        }
+        for (int i = 0; i < Integer.parseInt(args[5]); i++) {
+            Estadisticas.objetoRecogido("jugador", "comida");
+        }
+        for (int i = 0; i < Integer.parseInt(args[6]); i++) {
+            Estadisticas.objetoRecogido("jugador", "gema");
+        }
+        for (int i = 0; i < Integer.parseInt(args[7]); i++) {
+            Estadisticas.objetoRecogido("zarigueya", "comida");
+        }
+        for (int i = 0; i < Integer.parseInt(args[8]); i++) {
+            Estadisticas.objetoRecogido("zarigueya", "gema");
+        }
+        for (int i = 0; i < Integer.parseInt(args[9]); i++) {
+            Estadisticas.capturarDisparo(true);
+        }
+        for (int i = 0; i < Integer.parseInt(args[10]); i++) {
+            Estadisticas.capturarDisparo(false);
+        }
+        for (int i = 0; i < Integer.parseInt(args[11]); i++) {
+            Estadisticas.borrarDisparo(true);
+        }
+        for (int i = 0; i < Integer.parseInt(args[12]); i++) {
+            Estadisticas.borrarDisparo(false);
+        }
+        System.out.println("****************************************");
+        Estadisticas.mostrarEventosTeclado();
+        System.out.println("****************************************");
+        KeyCode code = Estadisticas.teclaMasPulsada();
+        if(code!=null) {
+            System.out.println("La tecla que mas veces se ha pulsado ha sido: " + code.toString());
+            System.out.println("****************************************");
+        }
+        Estadisticas.mostrarObjetosRecogidos();
+        System.out.println("****************************************");
+        Estadisticas.mostrarQuienHaRecogidoMasObjetos();
+        System.out.println("****************************************");
+        Estadisticas.mostrarRatioPrecision();
+        System.out.println("****************************************");
     }
 
 }
